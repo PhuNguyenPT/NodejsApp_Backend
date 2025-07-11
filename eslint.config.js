ed80c1e5d966b3c980f3a22246c9514e38daed92
phunpt <phu.npt01@gmail.com>
2025-07-11 11:08:05 +0700

    add vite test

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,11 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import perfectionist from "eslint-plugin-perfectionist";
+import vitest from "@vitest/eslint-plugin";
 
 export default tseslint.config(
   {
-    ignores: ["**/*.js", "dist/**/*", "node_modules/**/*", "**/*.d.ts"],
+    ignores: ["**/*.js", "dist/**/*"],
   },
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
@@ -20,4 +21,14 @@ export default tseslint.config(
     },
   },
   perfectionist.configs["recommended-natural"],
+  {
+    files: ["**/*.test.ts", "**/*.spec.ts"],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+      "@typescript-eslint/unbound-method": "off",
+    },
+  },
 );
