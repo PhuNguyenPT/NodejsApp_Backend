56d4d03ad9f274407b20d4d339be0c09c13b9cf3
phunpt <phu.npt01@gmail.com>
2025-07-11 10:00:01 +0700

    fix syntax

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,7 +5,7 @@ import express from "express";
 const app = express();
 const port = process.env.PORT ?? "9001";
 
-app.get("/", middleware as express.RequestHandler);
+app.get("/", middleware);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
