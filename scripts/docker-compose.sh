a0a3832dd5ed12841de7f8d4045522c94975b40d
phunpt <phu.npt01@gmail.com>
2025-07-22 15:06:08 +0700

    update .env and docker-compose to add redis

--- scripts/docker-compose.sh
+++ scripts/docker-compose.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker compose -f docker-compose.yml up -d
+
+# dev environment
+docker compose --env-file .env.dev -p node-app -f docker-compose.yml up -d
+
+# production environment
+# docker compose --env-file .env.prod -p cleaning-service -f docker-compose.yml up -d
+
+# staging environment
+# docker compose --env-file .env.staging -p cleaning-service -f docker-compose.yml up -d
