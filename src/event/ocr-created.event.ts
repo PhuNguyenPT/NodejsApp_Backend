df2472537e5a4aec97c16915c31bd15a98497123
phunpt <phu.npt01@gmail.com>
2025-12-04 12:07:31 +0700

    rename OCRCreatedEventSchema to OcrCreatedEventSchema

--- src/event/ocr-created.event.ts
+++ src/event/ocr-created.event.ts
@@ -1,9 +1,9 @@
 import z from "zod";
 
-export const OCRCreatedEventSchema = z.object({
+export const OcrCreatedEventSchema = z.object({
     ocrResultIds: z.array(z.string().uuid("Invalid OCR result ID format")),
     studentId: z.string().uuid("Invalid student ID format"),
     userId: z.string().uuid("Invalid user ID format").optional(),
 });
 
-export type OcrCreatedEvent = z.infer<typeof OCRCreatedEventSchema>;
+export type OcrCreatedEvent = z.infer<typeof OcrCreatedEventSchema>;
