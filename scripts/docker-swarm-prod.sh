d80b2ae6f18c3bff13d8801179e8e5e89f6e11d3
phunpt <phu.npt01@gmail.com>
2025-12-20 19:10:33 +0700

    add swarm prod

---
+++ scripts/docker-swarm-prod.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Start Backend stack for prod
+set -a
+source .env.prod
+set +a
+
+if ! docker network ls --format '{{.Name}}' | grep -q '^backend-swarm-network-prod$'; then
+    docker network create --driver overlay --attachable backend-swarm-network-prod
+fi
+
+if ! docker network ls --format '{{.Name}}' | grep -q '^frontend-swarm-network-prod'; then
+    docker network create --driver overlay --attachable frontend-swarm-network-prod
+fi
+
+if ! docker network ls --format '{{.Name}}' | grep -q '^app-shared-network-prod$'; then
+    docker network create --driver overlay --attachable app-shared-network-prod
+fi
+docker stack deploy -c docker-compose.swarm.prod.yml backend-swarm-prod --detach=false
